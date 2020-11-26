b1fe321d75c6bc95fcced383a48134d968e264f9
张吉祥 <zhangjixiang@zhangjiangdeMBP.lan>
2020-11-26 17:42:26 +0800

    update

--- src/pipeline/scripts/TaskTransfer.py
+++ src/pipeline/scripts/TaskTransfer.py
@@ -28,23 +28,19 @@ class TaskTransfer:
         return self.moveToClient.get_state() == actionlib.GoalStatus.SUCCEEDED
 
     def task_transfer(self, src_point, des_point):
+        '''
+        任务逻辑决策函数
+        '''
         with RobotCommander() as robot_commander:
             robot_commander.sendCordinate(
                 command_code=51,
                 x=src_point.getPosX(),
                 y=src_point.getPosY(),
-                yaw=src_point.getYaw(),
+                yaw=src_point.getYaw()
             )
             print ("sendCordinate")
-
         des_point.setPreTaskPoint(src_point)
 
-        # goal_msg = MoveToPosition2DGoal()
-        # goal_msg.header.stamp = rospy.Time.now()
-        # goal_msg.header.frame_id = "map"
-        # goal_msg.target_pose.x = des_point.getPosX()
-        # goal_msg.target_pose.y = des_point.getPosY()
-        # goal_msg.target_pose.theta = des_point.getYaw()
         goal_msg = MoveBaseGoal()
         goal_msg.target_pose.header.stamp = rospy.Time.now()
         goal_msg.target_pose.header.frame_id = "map"
@@ -57,36 +53,21 @@ class TaskTransfer:
         goal_msg.target_pose.pose.orientation.z = my_q[2]
         goal_msg.target_pose.pose.orientation.w = my_q[3]
 
-        # if des_point.is_speed_up():
-        #     goal_msg.target_transition_vel = 2.2
-        #     goal_msg.target_angle_vel = 0.5
-        #  else:
-        # goal_msg.target_transition_vel = 1.0
-        # goal_msg.target_angle_vel = 0.2
-
-        # if des_point.is_middle_point() and not (
-        #     des_point.is_down_stair() or des_point.is_up_stair()
-        # ):
-        #     print "##################"
-        #     print "wide pose range."
-        #     print "##################"
-        #     goal_msg.target_distance = 0.15
-        #     goal_msg.target_angle = 0.1
-        # else:
-        #     goal_msg.target_distance = 0.15
-        #     goal_msg.target_angle = 0.1
-
         print "Option:"
         print des_point.record["option"]
 
         not_done = True
 
         while not_done and not rospy.is_shutdown():
+            '''
+            执行两点间的任务，一直到成功或失败
+            '''
             self.moveToClient.send_goal(goal_msg)
             rospy.logwarn(
                 "Transfer ftom [%s] to [%s]" % (src_point.name, des_point.name)
             )
 
+            # crawl
             if des_point.getPreTaskPoint() and des_point.getPreTaskPoint().is_crawl():
                 rospy.sleep(0.5)
                 if self.plan_failed():
@@ -96,7 +77,7 @@ class TaskTransfer:
                 with RobotCommander() as robot_commander:
                     robot_commander.crawl_trait()
                     rospy.sleep(0.1)
-
+            # openDoor?
             if (
                 des_point.getPreTaskPoint()
                 and des_point.getPreTaskPoint().is_openDoor()
@@ -109,7 +90,7 @@ class TaskTransfer:
                 with RobotCommander() as robot_commander:
                     robot_commander.openDoor_trait()
                     rospy.sleep(0.1)
-
+            # leaveDoor?
             if (
                 des_point.getPreTaskPoint()
                 and des_point.getPreTaskPoint().is_leaveDoor()
@@ -122,7 +103,7 @@ class TaskTransfer:
                 with RobotCommander() as robot_commander:
                     robot_commander.leaveDoor_trait()
                     rospy.sleep(0.1)
-
+            # walking_bricks
             if (
                 des_point.getPreTaskPoint()
                 and des_point.getPreTaskPoint().is_walking_bricks()
@@ -135,7 +116,7 @@ class TaskTransfer:
                 with RobotCommander() as robot_commander:
                     robot_commander.walking_bricks_trait()
                     rospy.sleep(0.1)
-
+            # down_stair
             if (
                 des_point.getPreTaskPoint()
                 and des_point.getPreTaskPoint().is_down_stair()
@@ -148,7 +129,7 @@ class TaskTransfer:
                 with RobotCommander() as robot_commander:
                     robot_commander.down_stair_trait()
                     rospy.sleep(0.1)
-
+            # up_stair
             if (
                 des_point.getPreTaskPoint()
                 and des_point.getPreTaskPoint().is_up_stair()
@@ -161,15 +142,18 @@ class TaskTransfer:
                 with RobotCommander() as robot_commander:
                     robot_commander.up_stair_trait()
                     rospy.sleep(0.1)
+            
             done = self.moveToClient.wait_for_result(timeout=rospy.Duration(300.0))
+            
             # Make sure the action succeed.
             not_done = (not done) or (
                 self.moveToClient.get_state() != actionlib.GoalStatus.SUCCEEDED
             )
             print "Goal status: ", self.moveToClient.get_state() == actionlib.GoalStatus.SUCCEEDED
-            if not_done:
-                print "Goal not done, Resent goal. "
-
+        '''
+        两点间的任务执行完成
+        '''
+        # 到达任务点后结束下楼梯
         if (
             not self.plan_failed()
             and des_point.getPreTaskPoint()
@@ -180,7 +164,7 @@ class TaskTransfer:
                 robot_commander.finish_down_stair_trait()
                 rospy.sleep(0.1)
             rospy.sleep(0.5)
-
+        # 到达任务点后结束上楼梯
         if (
             not self.plan_failed()
             and des_point.getPreTaskPoint()
@@ -191,41 +175,7 @@ class TaskTransfer:
                 robot_commander.finish_up_stair_trait()
                 rospy.sleep(0.1)
             rospy.sleep(0.5)
-
-        # if not self.plan_failed() and  des_point.getPreTaskPoint() and des_point.getPreTaskPoint().is_openDoor():
-        #     print "Finish openDoor trait"
-        #     with RobotCommander() as robot_commander:
-        #         robot_commander.finish_openDoor_trait()
-        #         rospy.sleep(0.1)
-        #     rospy.sleep(0.5)
-
-        if (
-            not self.plan_failed()
-            and des_point.getPreTaskPoint()
-            and des_point.getPreTaskPoint().is_leaveDoor()
-        ):
-            print "Finish leaveDoor trait"
-            with RobotCommander() as robot_commander:
-                robot_commander.finish_leaveDoor_trait()
-                rospy.sleep(0.1)
-            rospy.sleep(0.5)
-
-        if not self.plan_failed() and des_point.is_autoCharge():
-            rospy.sleep(0.5)
-            if self.plan_failed():
-                rospy.logerr("Plan failed! Robot may stuck in this place.")
-                print "autoCharge"
-            with RobotCommander() as robot_commander:
-                robot_commander.autoCharge_trait()
-                rospy.sleep(0.5)
-
-        if not self.plan_failed() and des_point.is_autoCharge():
-            print "Finish autoCharge"
-            with RobotCommander() as robot_commander:
-                robot_commander.finish_autoCharge_trait()
-                rospy.sleep(0.1)
-            rospy.sleep(0.5)
-
+        # 到达任务点后用延时的方式上斜坡
         if not self.plan_failed() and des_point.is_climbSlope():
             rospy.sleep(0.5)
             if self.plan_failed():
@@ -234,62 +184,14 @@ class TaskTransfer:
             with RobotCommander() as robot_commander:
                 robot_commander.climbSlope_trait()
                 rospy.sleep(18)
-
+        
         if not self.plan_failed() and des_point.is_climbSlope():
             print "Finish climbSlope"
             with RobotCommander() as robot_commander:
                 robot_commander.finish_climbSlope_trait()
                 rospy.sleep(0.1)
             rospy.sleep(0.5)
-
-        if not self.plan_failed() and des_point.is_perform():
-            rospy.sleep(0.5)
-            if self.plan_failed():
-                rospy.logerr("Plan failed! Robot may stuck in this place.")
-                print "perform"
-            with RobotCommander() as robot_commander:
-                robot_commander.perform_trait()
-                rospy.sleep(18)
-
-        if not self.plan_failed() and des_point.is_perform():
-            print "Finish perform"
-            with RobotCommander() as robot_commander:
-                robot_commander.finish_perform_trait()
-                rospy.sleep(0.1)
-            rospy.sleep(0.5)
-
-        if not self.plan_failed() and des_point.is_jump1():
-            rospy.sleep(0.5)
-            if self.plan_failed():
-                rospy.logerr("Plan failed! Robot may stuck in this place.")
-                print "jump1"
-            with RobotCommander() as robot_commander:
-                robot_commander.jump1_trait()
-                rospy.sleep(0.5)
-
-        if not self.plan_failed() and des_point.is_jump1():
-            print "Finish jump1"
-            with RobotCommander() as robot_commander:
-                robot_commander.finish_jump1_trait()
-                rospy.sleep(0.1)
-            rospy.sleep(0.5)
-
-        if not self.plan_failed() and des_point.is_jump2():
-            rospy.sleep(0.5)
-            if self.plan_failed():
-                rospy.logerr("Plan failed! Robot may stuck in this place.")
-                print "jump2"
-            with RobotCommander() as robot_commander:
-                robot_commander.jump2_trait()
-                rospy.sleep(8)
-
-        if not self.plan_failed() and des_point.is_jump2():
-            print "Finish jump2"
-            with RobotCommander() as robot_commander:
-                robot_commander.finish_jump2_trait()
-                rospy.sleep(0.1)
-            rospy.sleep(0.5)
-
+        
         if (
             not self.plan_failed()
             and des_point.getPreTaskPoint()
