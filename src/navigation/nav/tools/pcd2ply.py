6aaf0e4b2852c5773c5a15767f66cf6bfe05d774
matheecs <1780162778@qq.com>
2021-02-26 10:55:40 +0800

    add tool for point cloud visualization

---
+++ src/navigation/nav/tools/pcd2ply.py
@@ -0,0 +1,9 @@
+import os
+import open3d as o3d
+
+map_pcd_path = str(os.path.dirname(os.path.abspath(__file__))) + "/../map/file.pcd"
+map_ply_path = str(os.path.dirname(os.path.abspath(__file__))) + "/../map/file.ply"
+map_pcd_data = o3d.io.read_point_cloud(map_pcd_path)
+o3d.visualization.draw_geometries([map_pcd_data])
+
+o3d.io.write_point_cloud(map_ply_path, map_pcd_data)
