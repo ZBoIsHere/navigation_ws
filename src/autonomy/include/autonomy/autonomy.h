8e6c6d11acf7b266ecdb535cd746471aa18696d3
matheecs <1780162778@qq.com>
2021-03-12 18:20:30 +0800

    添加 json 功能

--- src/autonomy/include/autonomy/autonomy.h
+++ src/autonomy/include/autonomy/autonomy.h
@@ -1,48 +1,53 @@
 #ifndef AUTONOMY_H
 #define AUTONOMY_H
 
-#include <ros/ros.h>
 #include <ros/console.h>
+#include <ros/ros.h>
 #include <rviz/panel.h>
 
 #include <QHBoxLayout>
+#include <QLabel>
 #include <QLineEdit>
 #include <QPushButton>
 #include <QTimer>
 #include <QVBoxLayout>
 
-class QPushButton;
+#include "autonomy/json.hpp"
 
-namespace autonomy
-{
-class Autonomy : public rviz::Panel
-{
-Q_OBJECT
-public:
+using json = nlohmann::json;
+
+class QPushButton;
+class QLabel;
+
+namespace autonomy {
+class Autonomy : public rviz::Panel {
+  Q_OBJECT
+ public:
   explicit Autonomy(QWidget* parent = 0);
 
   virtual void load(const rviz::Config& config);
   virtual void save(rviz::Config config) const;
 
-public Q_SLOTS:
+ public Q_SLOTS:
 
-protected Q_SLOTS:
+ protected Q_SLOTS:
+  void addOneWaypoint();
+  void saveAllWaypoints();
+  void runAutonomy();
+  void stopAutonomy();
 
-  void moveNext();
-
-  void moveAuto();
-
-  void moveFullAuto();
-
-  void moveStop();
-
-protected:
+ protected:
   QPushButton* btn_add_;
   QPushButton* btn_save_;
   QPushButton* btn_run_;
   QPushButton* btn_stop_;
-  QLineEdit* added_waypoint_;
-  QLineEdit* total_waypoint_;
+  QLabel* show_added_waypoint_;
+  QLabel* show_total_waypoint_;
+
+  json j_;
+  int from_id_;
+  int next_id_;
+  int count_waypoints_;
 };
 
 }  // end namespace autonomy
--- src/autonomy/src/autonomy.cpp
+++ src/autonomy/src/autonomy.cpp
@@ -1,97 +1,84 @@
-#include <cstdio>
-
-#include <QLabel>
-
 #include "autonomy/autonomy.h"
 
-namespace autonomy
-{
-Autonomy::Autonomy(QWidget* parent) : rviz::Panel(parent)
-{
-  // Create a push button
+#include <QLabel>
+#include <iostream>
+#include <string>
+
+namespace autonomy {
+Autonomy::Autonomy(QWidget* parent)
+    : rviz::Panel(parent), from_id_(0), next_id_(0), count_waypoints_(0) {
   btn_add_ = new QPushButton(this);
   btn_add_->setText("Add");
-  connect(btn_add_, SIGNAL(clicked()), this, SLOT(moveNext()));
-
-  // Create a push button
   btn_save_ = new QPushButton(this);
   btn_save_->setText("Save");
-  connect(btn_save_, SIGNAL(clicked()), this, SLOT(moveAuto()));
-
-  // Create a push button
   btn_run_ = new QPushButton(this);
   btn_run_->setText("Run");
-  connect(btn_run_, SIGNAL(clicked()), this, SLOT(moveFullAuto()));
-
-  // Create a push button
   btn_stop_ = new QPushButton(this);
   btn_stop_->setText("Stop");
-  connect(btn_stop_, SIGNAL(clicked()), this, SLOT(moveStop()));
 
-  // Horizontal Layout
-  auto* hlayout1 = new QHBoxLayout;
-  hlayout1->addWidget(btn_add_);
-  hlayout1->addWidget(btn_save_);
+  connect(btn_add_, SIGNAL(clicked()), this, SLOT(addOneWaypoint()));
+  connect(btn_save_, SIGNAL(clicked()), this, SLOT(saveAllWaypoints()));
+  connect(btn_run_, SIGNAL(clicked()), this, SLOT(runAutonomy()));
+  connect(btn_stop_, SIGNAL(clicked()), this, SLOT(stopAutonomy()));
 
-  auto* hlayout2 = new QHBoxLayout;
-  hlayout2->addWidget(btn_run_);
-  hlayout2->addWidget(btn_stop_);
+  show_added_waypoint_ = new QLabel("0");
+  show_total_waypoint_ = new QLabel("0");
 
-  auto* left_layout = new QHBoxLayout;
-  left_layout->addWidget( new QLabel( "Added:" ));
-  added_waypoint_ = new QLineEdit;
-  left_layout->addWidget( added_waypoint_ );
-
-  auto* right_layout = new QHBoxLayout;
-  right_layout->addWidget( new QLabel( "Total:" ));
-  total_waypoint_ = new QLineEdit;
-  right_layout->addWidget( total_waypoint_ );
+  auto* top = new QHBoxLayout;
+  top->addWidget(btn_add_);
+  top->addWidget(btn_save_);
 
   auto* mid = new QHBoxLayout;
-  mid->addLayout(left_layout);
-  mid->addLayout(right_layout);
+  mid->addWidget(new QLabel("Added:"));
+  mid->addWidget(show_added_waypoint_);
+  mid->addWidget(new QLabel("Total:"));
+  mid->addWidget(show_total_waypoint_);
 
-  // Verticle layout
+  auto* bot = new QHBoxLayout;
+  bot->addWidget(btn_run_);
+  bot->addWidget(btn_stop_);
+
   auto* layout = new QVBoxLayout;
-  layout->addLayout(hlayout1);
+  layout->addLayout(top);
   layout->addLayout(mid);
-  layout->addLayout(hlayout2);
+  layout->addLayout(bot);
   setLayout(layout);
 
   btn_add_->setEnabled(true);
   btn_save_->setEnabled(true);
   btn_run_->setEnabled(true);
+  btn_stop_->setEnabled(true);
 }
 
-void Autonomy::moveNext()
-{
-  // remote_reciever_.publishNext();
+void Autonomy::addOneWaypoint() {
+  next_id_ = from_id_ + 1;
+  j_[std::to_string(from_id_)]["x"] = 1.0;
+  j_[std::to_string(from_id_)]["y"] = 2.0;
+  j_[std::to_string(from_id_)]["theta"] = 3.0;
+  j_[std::to_string(from_id_)]["next"] = next_id_;
+  ++from_id_;
+  ++count_waypoints_;
+
+  std::string added_id = std::to_string(from_id_);
+  QString added_str = QString::fromUtf8(added_id.c_str());
+  show_added_waypoint_->setText(added_str);
+
+  std::string count_id = std::to_string(count_waypoints_);
+  QString count_str = QString::fromUtf8(count_id.c_str());
+  show_total_waypoint_->setText(count_str);
 }
 
-void Autonomy::moveAuto()
-{
-  // remote_reciever_.publishContinue();
+void Autonomy::saveAllWaypoints() {
+  std::cout << std::setw(4) << j_ << std::endl;
 }
 
-void Autonomy::moveFullAuto()
-{
-  // remote_reciever_.publishBreak();
-}
+void Autonomy::runAutonomy() {}
 
-void Autonomy::moveStop()
-{
-  // remote_reciever_.publishStop();
-}
+void Autonomy::stopAutonomy() {}
 
-void Autonomy::save(rviz::Config config) const
-{
-  rviz::Panel::save(config);
-}
+void Autonomy::save(rviz::Config config) const { rviz::Panel::save(config); }
 
-void Autonomy::load(const rviz::Config& config)
-{
-  rviz::Panel::load(config);
-}
+void Autonomy::load(const rviz::Config& config) { rviz::Panel::load(config); }
 }  // end namespace autonomy
 
 #include <pluginlib/class_list_macros.h>
